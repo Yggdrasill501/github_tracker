df2f97cd85c337d759ff65e4c8d7e41ad1e720d8
Filip Žitný <filipzitny@gmail.com>
2024-04-24 19:15:07 +0200

    fix urls

--- github_tracker/urls.py
+++ github_tracker/urls.py
@@ -15,9 +15,17 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
+from django.views.generic import RedirectView
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('events.urls'))
+]
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('events/', include('events.urls')),
+    re_path(r'^$', RedirectView.as_view(url='/events/', permanent=False)), 
 ]
