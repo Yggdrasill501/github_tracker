df322c097a8cd316424fe46174177b82372c1c43
Filip Žitný <filipzitny@gmail.com>
2024-04-26 11:15:11 +0200

    test

--- api/tests.py
+++ api/tests.py
@@ -5,6 +5,6 @@ from rest_framework.test import APITestCase
 
 class GitHubEventAPITests(APITestCase):
     def test_retrieve_events(self):
-        url = reverse('github-events')
+        url = reverse('github-events/')
         response = self.client.get(url)
         self.assertEqual(response.status_code, status.HTTP_200_OK)
